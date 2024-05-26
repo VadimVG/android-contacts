43dfbdd32240d74d1b9ac68cfe2162630a28a547
VadimVG <gucuvadim057@gmail.com>
2024-05-26 12:35:02 +0300

    Яндекс практикум. Проектная работа №2. Попытка №2.1.

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
@@ -8,7 +8,7 @@ public class BaseListDiffCallback<T extends ListDiffInterface<T> > extends DiffU
 
     @Override
     public boolean areItemsTheSame(@NonNull T t, @NonNull T t1) {
-        return t.theSameAs(t);
+        return t.theSameAs(t1);
     }
 
     @Override
